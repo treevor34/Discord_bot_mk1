ec6c7e725dada784a8c82c40588d95e45e81654e
treevor34 <46980500+treevor34@users.noreply.github.com>
2021-12-13 13:32:02 -0500

    Delete silly.js

--- commands/silly.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: "this is a ping command!",
-    execute(message, args){
-        message.channel.send('Ping');
-        message.channel.send('Damn wait, pong.');
-    }
-}
